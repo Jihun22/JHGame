fc44988ee119130cdeb0000f0682404020241571
YANGJIHUN <ik5334@naver.com>
2023-11-17 17:12:26 +0900

    위아래 세팅

--- Assets/Scripts/Background.cs
+++ Assets/Scripts/Background.cs
@@ -10,5 +10,11 @@ public class Background : MonoBehaviour
     {
      transform.position += Vector3.down * moveSpeed *Time.deltaTime;
 
+     if(transform.position.y < -10) {
+
+        transform.position += new Vector3(0,20f,0);
+
+     }
+
     }
 }
