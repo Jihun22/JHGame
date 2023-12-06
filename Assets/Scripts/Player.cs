3eb57e88f176c42cc2391d510d13824ab3db1a03
YANGJIHUN <ik5334@naver.com>
2023-12-06 20:46:59 +0900

    마우스로 제어

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,6 +14,8 @@ public class Player : MonoBehaviour
     //  Vector3 moveTo = new Vector3(horizontalInput,verticalInput,0f);
 
     //  transform.position += moveTo * moveSpeed * Time.deltaTime;
+
+    /*키보드 제어
     Vector3 moveTo = new Vector3(moveSpeed * Time.deltaTime,0,0);
     if(Input.GetKey(KeyCode.LeftArrow)){
         transform.position -= moveTo;
@@ -21,6 +23,10 @@ public class Player : MonoBehaviour
     } else if (Input.GetKey(KeyCode.RightArrow)) {
         transform.position += moveTo;
     }
-
+   */
+   //마우스 제어 
+    // 마우스 로그 Debug.Log(Input.mousePosition);
+    Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    Debug.Log(Input.mousePosition);
     }
 }
